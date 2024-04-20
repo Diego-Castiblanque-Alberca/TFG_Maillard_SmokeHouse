63b6a56077af863ffedc8e6ef22b01ebe5a638dd
marcos <mcachafeiro@gmail.com>
2024-04-20 10:12:49 +0200

    implementacion estilos reservapaso4

--- front-end/src/views/reservar/ReservaPaso4.jsx
+++ front-end/src/views/reservar/ReservaPaso4.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import paso from "../../imgs/paso4.svg";
+import check from "../../imgs/check.svg"
 import InfoReserva from "../../components/reservar/InfoReserva";
 import "../../styles/reservar/ReservaPaso4.css";
 
@@ -9,9 +10,13 @@ export default function ReservaPaso4({datos}) {
     return (
         <>
             <img src={paso} alt=""/>
+            <div className="reserva-confirmada">
+                <img src={check} alt="" />
+                <h2>Reserva confirmada</h2>
+            </div>
             <div className="contenedor-datos-reserva">
                 <InfoReserva datos={datos}/>
-                <h3>Reserva a nombre de:&nbsp;&nbsp;<span>{nombre} {apellidos}</span></h3>
+                <h3 className="nombre-reserva">Reserva a nombre de:&nbsp;&nbsp;<span>{nombre} {apellidos}</span></h3>
                 <div className="texto-agradecimiento">
                     <p>Gracias por reservar en Maillard SmokeHouse.</p>
                     <p>Recibirá en breve un email de confirmación.</p>
