1536d29ddf6056570d6b23ef8ff1bacbbfc5baf3
marcos <mcachafeiro@gmail.com>
2024-05-28 19:29:08 +0200

    Update CORS configuration and web route in back-end

---
+++ back-end/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maillard SmokeHouse</title>
+    <link rel="stylesheet" href="{{ asset('assets/index-BxdQn77y.css') }}">
+</head>
+<body>
+    <div id="root"></div>
+    <script src="{{ asset('assets/index-BxdQn77y.js') }}"></script>
+</body>
+</html>
+
